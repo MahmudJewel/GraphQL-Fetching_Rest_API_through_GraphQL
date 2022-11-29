a87eb47c4371cd6d2e56fbb937e6c48be1ac9673
Mahmud <jmbengal.mbstu@gmail.com>
2022-11-29 14:49:24 +0600

    data fetched using node-fetch and converting on GraphQL

--- resolver.js
+++ resolver.js
@@ -1,8 +1,14 @@
+import fetch from 'node-fetch';
 
 const resolvers = {
-    Query:{
-        hello:()=> "Hello world!",
+    Query: {
+        hello: () => "Hello world!",
+        getComment: async (_, { id }) => {
+            const response = await fetch(`https://jsonplaceholder.typicode.com/comments/${id}/`);
+            return response.json();
+        }
     }
+
 }
 
 export default resolvers;
--- schema.js
+++ schema.js
@@ -1,6 +1,15 @@
 const typeDefs = `
     type Query{
         hello: String
+        getComment(id: Int!): Comments
+    }
+
+    type Comments{
+        postId: Int,
+        id: Int,
+        name: String,
+        email: String,
+        body: String
     }
 `;
 
